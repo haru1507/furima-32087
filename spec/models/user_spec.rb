1a926543e63bd97b376d0f13e2cb38498e334886
haru1507 <haruki.baske1226@gmail.com>
2020-12-16 14:34:02 +0900

    テストコード４

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
-      it 'nicknameとemail、passwordとpassword_confirmation,last_nameとfirst_name,last_name_kanaとfirst_name_kana,birth_dateが存在すれば登録できる' do
+      it 'nicknameとemail、passwordとpassword_confirmation,last_nameと
+      　first_name,last_name_kanaとfirst_name_kana,birth_dateが存在すれば登録できる' do
         expect(@user).to be_valid
       end
     end
