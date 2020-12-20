24ff479efd16482e89c2b82445a9d105eddd8533
haru1507 <haruki.baske1226@gmail.com>
2020-12-20 16:22:03 +0900

    active_hashの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:info,:category_id,:sales_status_id,:shipping_fee_status_id,:prefecture_id,:scheduled_delivary_id,:price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category_id
+  belongs_to :sales_status_id
+  belongs_to :shipping_fee_status_id
+  belongs_to :prefecture_id
+  belongs_to :scheduled_delivary_id
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivary_id
+  end
+
 end
