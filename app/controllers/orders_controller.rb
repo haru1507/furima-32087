b75510ec4ea2ff9d9eb46d114dcfb66f82fda4b0
haru1507 <haruki.baske1226@gmail.com>
2021-01-14 12:49:45 +0900

    エラーメッセージの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,25 @@
 class OrdersController < ApplicationController
+
   def index
     @user_order = UserOrder.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @user_order = UserOrder.new(order_params)
-    if @user_order.valid?
-      @user_order.save
-      redirect_to action: :index
-    else
-      render action: :new
-    end
+    @item = Item.find(params[:item_id])
+      if @user_order.valid?
+        @user_order.save
+        redirect_to root_path
+      else
+        render 'index'
+      end
   end
 
   private
 
   def order_params
-    params.require(:user_order).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,6 +1,6 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
@@ -11,10 +11,8 @@ class UserOrder
   end
 
   def save
-    user = User.create(first_name: first_name, last_name: last_name, first_name_kana: first_name_kana, last_name_kana: last_name_kana, nickname: nickname, birth_date: birth_date)
-    item = Item.create(name: name, info: info, price: price, image: image, category_id: category_id, sales_status_id: sales_status_id, shipping_fee_status_id: shipping_fee_status_id, prefecture_id: prefecture_id, scheduled_delivery_id: scheduled_delivery_id)
-    Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 
 end
