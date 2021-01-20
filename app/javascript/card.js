33a99ee61730853471056bf82fff0fed16a7cb6f
haru1507 <haruki.baske1226@gmail.com>
2021-01-20 15:12:12 +0900

    クライアントサイドの環境変数設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_5b4a2500a6ff9496c1a02fe9");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
