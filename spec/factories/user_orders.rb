e906a6b58194c29e76fad2b7a7eaabc16e40b639
haru1507 <haruki.baske1226@gmail.com>
2021-01-24 14:20:05 +0900

    テストコードの修正２

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -4,9 +4,8 @@ FactoryBot.define do
     prefecture_id { 2 }
     city          { '北見市' }
     addresses     { '青山1-1-1' }
+    building      { '横浜プラザ'}
     phone_number  { '09012345678' }
     token         { 'tok_abcdefghijk0123456789' }
-    user_id       { 1 }
-    item_id       { 1 }
   end
 end
